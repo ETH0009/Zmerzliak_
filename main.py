d024955f37769b2f3002c13a08650e4f9420d52c
ETH0009 <flaflakovic@gmail.com>
2025-08-08 09:41:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from send_email import send_email
 from PIL import Image
 
-
+image = Image.open("/rr.JPG")
 st.title('Zmerzliak for today')
 new_todo= st.text_input(label='Enter text', key='new_todo' )
-st.image("/rr.JPG", use_column_width=True)
+st.image(image)
 send_email(message=new_todo)
